e2bbfa873b91c297cbb1c496df0a2085d4450d9b
Hello911 <gongweichen1998@gmail.com>
2018-10-17 12:41:41 -0400

    added more cases in Main

---
+++ app/src/main/java/sidebyside3/david/com/sidebyside4/Setting.java
@@ -0,0 +1,18 @@
+package sidebyside3.david.com.sidebyside4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import app.akexorcist.bluetotohspp.library.BluetoothSPP;
+
+public class Setting extends AppCompatActivity {
+    BluetoothSPP bt;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.setting);
+
+    }
+
+}
